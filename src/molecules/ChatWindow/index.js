7fe12e48b66e013b6208959ab9334f8406149a88
Gabriel Araujo <gabriela@copel.com>
2025-01-08 12:08:44 -0300

    adcionei o chat externo

--- src/molecules/ChatWindow/index.js
+++ src/molecules/ChatWindow/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './styles.css';
 import { Container } from 'react-bootstrap';
-import Copelo from '../../img/Copelo.png';
-import { FaUser } from "react-icons/fa6";  
-import { HiVolumeUp } from "react-icons/hi";  
-import { HiOutlineDuplicate } from "react-icons/hi";  
-import { BsPersonFill } from "react-icons/bs";
-import { BsCopy } from "react-icons/bs"; // Ícone de copiar adicionado
+import { HiVolumeUp, HiOutlineDuplicate } from "react-icons/hi";  
+import { BsPersonFill, BsHandThumbsUp, BsHandThumbsDown } from "react-icons/bs";
 import { TbRobot } from "react-icons/tb";
 
 export const ChatWindow = ({ messages, handleRatings }) => {
+  
+    // Função para copiar o conteúdo da mensagem para a área de transferência
+    const handleCopyMessage = (text) => {
+        navigator.clipboard.writeText(text)
+            .then(() => {
+                alert("Mensagem copiada!"); // Pode substituir o alert por outra interação visual, se desejar
+            })
+            .catch((err) => {
+                console.error('Falha ao copiar: ', err);
+            });
+    };
+
     return (
         <Container fluid className="chat-window">
             {messages.map((msg, index) => (
@@ -19,30 +27,40 @@ export const ChatWindow = ({ messages, handleRatings }) => {
                     ) : (
                         <TbRobot alt="Logo" className="logo-message" />
                     )}
-                                    
+                   
                     <div className="message-content">
                         <div className={`message-box ${msg.sender}`}>
                             <span>{msg.text}</span>
                             {msg.sender === 'bot' && <HiVolumeUp className="volume-icon" />}
                         </div>
-                        {msg.sender === 'bot' && msg.hasMenu && (
+ 
+                        {msg.sender === 'bot' && (
                             <div className='message-icons'>
-                                <button className='botaoIcone2' onClick={() => navigator.clipboard.writeText(msg.text)}>
+                               
+                                <button
+                                    className='botaoIcone2'
+                                    onClick={() => handleCopyMessage(msg.text)} // Chama a função de cópia
+                                    title="Copiar texto"
+                                >
                                     <HiOutlineDuplicate className="duplicate-icon" />
                                 </button>
-                                <button className='botaoIcone2' onClick={() => handleRatings(false)}>
-                                    <i className='pi pi-thumbs-down' />
+                                <button
+                                    className='botaoIcone2'
+                                    onClick={() => handleRatings(true)}
+                                    title="Gostei"
+                                >
+                                    <BsHandThumbsUp className="thumbs-up-icon" />
                                 </button>
-                                <button className='botaoIcone2' onClick={() => handleRatings(true)}>
-                                    <i className='pi pi-thumbs-up' />
-                                </button>
-                                <button className='botaoIcone2' onClick={() => navigator.clipboard.writeText(msg.text)}>
-                                    <BsCopy className="copy-icon" /> {/* Botão de copiar */}
+                                <button
+                                    className='botaoIcone2'
+                                    onClick={() => handleRatings(false)}
+                                    title="Não gostei"
+                                >
+                                    <BsHandThumbsDown className="thumbs-down-icon" />
                                 </button>
                             </div>
                         )}
-                    </div>              
-                    <div className='empty-space'></div>
+                    </div>
                 </div>
             ))}
         </Container>
